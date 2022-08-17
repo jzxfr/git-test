f57158c2c86dce4850e2da86e4caf5b79a5ba6a0
jinzhuxin <3160654609@qq.com>
2022-08-17 11:14:52 +0800

    hot-fix 2

--- src/main/java/git_test/git_test.java
+++ src/main/java/git_test/git_test.java
@@ -2,8 +2,8 @@ package git_test;
 
 public class git_test {
     public static void main(String[] args) {
-        System.out.println("hello git");
-        System.out.println("hello git");
-        System.out.println("hello git");
+        System.out.println("hello git1");
+        System.out.println("hello git2");
+        System.out.println("hello git3");
     }
 }
