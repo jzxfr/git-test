7ef975bf656abcce91ae478220d5ebdabacdca1a
jinzhuxin <3160654609@qq.com>
2022-08-17 13:48:18 +0800

    master 3

--- src/main/java/git_test/git_test.java
+++ src/main/java/git_test/git_test.java
@@ -8,5 +8,6 @@ public class git_test {
         System.out.println("hello git4");
         System.out.println("hello git5");
         System.out.println("hello git6");
+        System.out.println("hello git7");
     }
 }
