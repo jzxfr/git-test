470f7d4f829dd942c5ea9a75445a10574b87b4cf
jinzhuxin <3160654609@qq.com>
2022-08-17 11:16:09 +0800

    master 1

--- src/main/java/git_test/git_test.java
+++ src/main/java/git_test/git_test.java
@@ -2,8 +2,8 @@ package git_test;
 
 public class git_test {
     public static void main(String[] args) {
-        System.out.println("hello git");
-        System.out.println("hello git");
-        System.out.println("hello git");
+        System.out.println("hello git4");
+        System.out.println("hello git5");
+        System.out.println("hello git6");
     }
 }
